86bafbf74c34a6b2adb9891a23d195670f861661
powerrampage <powerrampage8484@gmail.com>
2025-11-09 13:09:38 +0500

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { default as MapSvgD3 } from "./map";
+export { default } from "./map";
+export * from "./types";
+export * from "./constants";
+export * from "./locales";
+export { getRegionName, regionTranslations } from "./locales";
+export type { RegionTranslations } from "./locales";
+
+import "./map.css";
+
